b3432f946c204eab35991268ff1d2c9cf0075643
Anatoly Ktitarov <hello@ktitarov.com>
2023-10-07 22:05:27 +0300

    Added readme

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # NetSpeed
+
+Utility that helps to measure network performance metrics using cURL ✨ 
+While its not recommended to rely on this tool for getting insights about your frontends ([read more](https://blog.cloudflare.com/ttfb-is-not-what-it-used-to-be/)), its very handy to measure your backends or microservices network stats.
+
+## How to run it
+
+```sh
+$ yarn install
+$ yarn start -u https://example.com -n 5
+```
+
+## Network metrics
+
+```json
+{
+  "time_total": {
+    "min": 0.787695,
+    "max": 1.470299,
+    "avg": 0.8801224800000002
+  },
+  "time_connect": {
+    "min": 0.198321,
+    "max": 0.29705,
+    "avg": 0.21884231999999992
+  },
+  "time_redirect": {
+    "min": 0.370254,
+    "max": 0.837215,
+    "avg": 0.4419513000000002
+  },
+  "time_appconnect": {
+    "min": 0.603826,
+    "max": 1.16023,
+    "avg": 0.6699872599999999
+  },
+  "time_namelookup": {
+    "min": 0.006924,
+    "max": 0.078889,
+    "avg": 0.013042020000000003
+  },
+  "time_pretransfer": {
+    "min": 0.604491,
+    "max": 1.16099,
+    "avg": 0.6707111600000001
+  },
+  "time_starttransfer": {
+    "min": 0.786604,
+    "max": 1.470005,
+    "avg": 0.8793156299999998
+  }
+}
+```
--- netspeed.mjs
+++ netspeed.mjs
@@ -1,24 +1,63 @@
 import 'zx/globals'
 
-const url = 'https://ozon.ru/'
 const template = `{
-  "time_appconnect": %{time_appconnect},
+  "time_total": %{time_total},
   "time_connect": %{time_connect},
+  "time_redirect": %{time_redirect},
+  "time_appconnect": %{time_appconnect},
   "time_namelookup": %{time_namelookup},
   "time_pretransfer": %{time_pretransfer},
-  "time_redirect": %{time_redirect},
-  "ttfb": %{time_starttransfer},
-  "ttlb": %{time_total}
+  "time_starttransfer": %{time_starttransfer}
 }`
 
-const output = await $`
-  curl -L \
-    --silent \
-    --show-error \
-    --output /dev/null \
-    --write-out ${template} \
-    ${url}
-`
+async function main() {
+  if (!argv.u) {
+    let output = 'Target url is required! '
+    output += 'Please, provide the target '
+    output += 'url using "-u" argument.'
+    console.error(output)
+    process.exit(1)
+  }
 
-const data = JSON.parse(output.stdout)
-console.log(data)
+  const url = argv.u
+  const num = (argv.n ?? 3)
+  console.log(`
+    \rPreparing to analyze...
+    \rTarget URL: "${url}"
+    \rIterations: "${num}"
+  `)
+
+  const result = {}
+
+  await spinner(async () => {
+    const records = []
+    for (let i = 0; i < num; i++) {
+      const cmd = $`curl -L -S -s -o /dev/null -w ${template} ${url}`
+      const res = await retry(3, () => cmd.quiet())
+      records.push(JSON.parse(res.stdout))
+    }
+  
+    const aggregate = {}
+    for (const record of records) {
+      const entries = Object.entries(record)
+      for (const [key, val] of entries) {
+        aggregate[key] = (aggregate[key] ?? [])
+        aggregate[key].push(val)
+      }
+    }
+  
+    for (const key in aggregate) {
+      const sum = (a, b) => a + b
+      const len = aggregate[key].length
+      const min = Math.min(...aggregate[key])
+      const max = Math.max(...aggregate[key])
+      const avg = aggregate[key].reduce(sum)/len
+      result[key] = { min, max, avg }
+    }
+  })
+
+  console.log(JSON.stringify(
+    result, null, 2))
+}
+
+main()
